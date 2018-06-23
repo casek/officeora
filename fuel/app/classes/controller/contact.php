8e11322c0d2e567afe12dff41dca4129b8baa782
Keisuke Mutoh <kmutoh@office-ora.com>
2018-06-23 14:02:01 +0900

    change method for sending mail to using SMTP.

--- fuel/app/classes/controller/contact.php
+++ fuel/app/classes/controller/contact.php
@@ -13,15 +13,24 @@ class Controller_Contact extends Controller_Restbase
 
             $message = $_POST['name']."\n".$_POST['email']."\n==========\n".$_POST['subject'].(($_POST['services']!='')?"\n".$_POST['services']:"")."\n\n".$_POST['message'];
 
-            $ret = mb_send_mail('kmutoh@gmail.com','オフィスORAサイトから問い合わせ',$message,'From: noreply@office-ora.com');
-            if(!$ret) {
-                throw new Exception();
-            }
+            $email = Email::forge();
+            $email->from('info@office-ora.com', 'Office ORA');
+            $email->to('kmutoh@office-ora.com');
+            $email->subject('オフィスORAサイトから問い合わせ');
+            $email->body($message);
+
+            $email->send();
+
+            //$ret = mb_send_mail('kmutoh@gmail.com','オフィスORAサイトから問い合わせ',$message,'From: noreply@office-ora.com');
+            //if(!$ret) {
+            //    throw new Exception();
+            //}
 
             $this->response(array(
                 'result' => true,
             ));
         } catch(Exception $e) {
+            Log::debug($e->getMessage());
             $this->response(array(
                 'result' => false,
             ));
